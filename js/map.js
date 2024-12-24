112e64dc7d277b7627b2ffc6234a639ca57201a4
ennurijo <ennurijo@gmail.com>
2024-12-24 12:58:30 -0500

    Update map.js
    updated again with 12/24 GPT

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([48.8575, 2.3514], 13); 
+const map = L.map('map').setView([48.8566, 2.3522], 13); // Set view to Paris, France
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
