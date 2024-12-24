7c72120f8f1c2d7af1c3779cfe3afc7c81f70fef
ennurijo <ennurijo@gmail.com>
2024-12-24 12:09:59 -0500

    Update searchable_map_lib.js
    Hover Content:
    
    Matches the structure of hover.ejs.
    Dynamically displays videos, images, and other record data.
    Search Functionality:
    
    Filters markers based on the search-name and search-address fields.
    Map and Marker Management:
    
    Uses leaflet.js for mapping and marker clustering.
    Dynamically updates markers based on search results.
    CSV Loading:
    
    Uses Papa.parse to load CSV data and extract relevant fields.

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -1,358 +1,131 @@
-var SearchableMapLib = SearchableMapLib || {};
-var SearchableMapLib = {
+var SearchableMapLib = (function () {
+  var map, markers, radiusCircle;
+  var mapConfig = {};
+  var dataRecords = [];
+  var infoWindow;
 
-  // parameters to be defined on initialize() 
-  map_centroid: [],
-  defaultZoom: 9,
-  filePath: '',
-  fileType: '',
-  csvOptions: '',
-  listOrderBy: '',
-  recordName: '',
-  recordNamePlural: '',
-  debug: false,
+  function initialize(config) {
+    mapConfig = config;
+    map = L.map('mapCanvas').setView(mapConfig.map_centroid, mapConfig.defaultZoom);
 
-  // internal properties
-  radius: '',
-  csvData: null,
-  geojsonData: null,
-  currentResults: null,
-  currentResultsLayer: null,
-  currentPinpoint: null,
-  lastClickedLayer: null,
+    // Add tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-  initialize: function(options){
-    options = options || {};
+    markers = L.markerClusterGroup();
+    radiusCircle = L.circle(mapConfig.map_centroid, {
+      radius: mapConfig.defaultRadius,
+      color: '#0073e6',
+      fillColor: '#0073e6',
+      fillOpacity: 0.2
+    }).addTo(map);
 
-    SearchableMapLib.map_centroid = options.map_centroid || [-48.87661025066399, -123.39330647791094],
-    SearchableMapLib.defaultZoom = options.defaultZoom || 9,
-    SearchableMapLib.filePath = options.filePath || "",
-    SearchableMapLib.fileType = options.fileType || "csv",
-    SearchableMapLib.csvOptions = options.csvOptions || {separator: ',', delimiter: '"'},
-    SearchableMapLib.listOrderBy = options.listOrderBy || "",
-    SearchableMapLib.recordName = options.recordName || "result",
-    SearchableMapLib.recordNamePlural = options.recordNamePlural || "results",
-    SearchableMapLib.radius = options.defaultRadius || 805,
-    SearchableMapLib.debug = options.debug || false
+    infoWindow = L.popup();
 
-    if (SearchableMapLib.debug)
-      console.log('debug mode is on');
+    loadData();
+  }
 
-    //reset filters
-    $("#search-address").val(SearchableMapLib.convertToPlainString($.address.parameter('address')));
+  function loadData() {
+    var filePath = mapConfig.filePath;
+    var fileType = mapConfig.fileType;
 
-    var loadRadius = SearchableMapLib.convertToPlainString($.address.parameter('radius'));
-    if (loadRadius != "") 
-        $("#search-radius").val(loadRadius);
-    else 
-        $("#search-radius").val(SearchableMapLib.radius);
-
-    $(":checkbox").prop("checked", "checked");
-
-    geocoder = new google.maps.Geocoder();
-    // initiate leaflet map
-    if (!SearchableMapLib.map) {
-      SearchableMapLib.map = new L.Map('mapCanvas', {
-        center: SearchableMapLib.map_centroid,
-        zoom: SearchableMapLib.defaultZoom,
-        scrollWheelZoom: false
-      });
-
-      SearchableMapLib.google = L.gridLayer.googleMutant({type: 'roadmap' });
-
-      SearchableMapLib.map.addLayer(SearchableMapLib.google);
-
-      //add hover info control
-      SearchableMapLib.info = L.control({position: 'bottomleft'});
-
-      SearchableMapLib.info.onAdd = function (map) {
-          this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-          this.update();
-          return this._div;
-      };
-
-      // method that we will use to update the control based on feature properties passed
-      var hover_template;
-      $.get( "../templates/hover.ejs", function( template ) {
-        hover_template = template;
-      });
-      SearchableMapLib.info.update = function (props) {
-        if (props) {
-          this._div.innerHTML = ejs.render(hover_template, {obj: props});
-        }
-        else {
-          this._div.innerHTML = 'Hover over a ' + SearchableMapLib.recordName;
-        }
-      };
-
-      SearchableMapLib.info.clear = function(){
-        this._div.innerHTML = 'Hover over a ' + SearchableMapLib.recordName;
-      };
-
-      //add results control
-      SearchableMapLib.results_div = L.control({position: 'topright'});
-
-      SearchableMapLib.results_div.onAdd = function (map) {
-        this._div = L.DomUtil.create('div', 'results-count');
-        this._div.innerHTML = "";
-        return this._div;
-      };
-
-      SearchableMapLib.results_div.update = function (count){
-        var recname = SearchableMapLib.recordNamePlural;
-        if (count == 1) {
-            recname = SearchableMapLib.recordName;
-        }
-
-        this._div.innerHTML = count.toLocaleString('en') + ' ' + recname + ' found';
-      };
-
-      SearchableMapLib.results_div.addTo(SearchableMapLib.map);
-      SearchableMapLib.info.addTo(SearchableMapLib.map);
-
-      $.when($.get(SearchableMapLib.filePath)).then(
-      function (data) {
-
-        if (SearchableMapLib.fileType == 'geojson') {
-          if (SearchableMapLib.debug) console.log('loading geojson');
-          // sometimes the server returns the file as text and we have to parse it
-          if (typeof data == 'string')
-            SearchableMapLib.geojsonData = JSON.parse(data);
-          else
-            SearchableMapLib.geojsonData = data
-        }
-        else if (SearchableMapLib.fileType == 'csv' ){
-          // convert CSV
-          if (SearchableMapLib.debug) console.log('converting to csv');
-          SearchableMapLib.geojsonData = convertCsvToGeojson(data)
-        }
-        else {
-          // error!
-          console.log ("fileType must be 'csv' or 'geojson'")
-        }
-
-        if (SearchableMapLib.debug) {
-          console.log('data loaded:');
-          console.log(SearchableMapLib.geojsonData);
-        }
-
-        SearchableMapLib.doSearch();
-
-      });
-    }
-  },
-
-  doSearch: function() {
-    SearchableMapLib.clearSearch();
-    var address = $("#search-address").val();
-    SearchableMapLib.radius = $("#search-radius").val();
-
-    if (SearchableMapLib.radius == null && address != "") {
-      SearchableMapLib.radius = 805;
-    }
-
-    if (address != "") {
-
-      geocoder.geocode( { 'address': address }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          SearchableMapLib.currentPinpoint = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
-          $.address.parameter('address', encodeURIComponent(address));
-          $.address.parameter('radius', SearchableMapLib.radius);
-          SearchableMapLib.address = address;
-          SearchableMapLib.createSQL(); // Must call create SQL before setting parameters.
-          SearchableMapLib.setZoom();
-          SearchableMapLib.addIcon();
-          SearchableMapLib.addCircle();
-          SearchableMapLib.renderMap();
-          SearchableMapLib.renderList();
-          SearchableMapLib.getResults();
-        }
-        else {
-          alert("We could not find your address: " + status);
-        }
-      });
-    }
-    else { //search without geocoding callback
-      SearchableMapLib.map.setView(new L.LatLng( SearchableMapLib.map_centroid[0], SearchableMapLib.map_centroid[1] ), SearchableMapLib.defaultZoom)
-      SearchableMapLib.createSQL(); // Must call create SQL before setting parameters.
-      SearchableMapLib.renderMap();
-      SearchableMapLib.renderList();
-      SearchableMapLib.getResults();
-    }
-
-  },
-
-  renderMap: function() {
-    SearchableMapLib.currentResultsLayer.addTo(SearchableMapLib.map);
-  },
-
-  renderList: function() {
-    var results = $('#results-list');
-    results.empty();
-
-    if (SearchableMapLib.currentResults.features.length == 0) {
-      results.append("<p class='no-results'>No results. Please broaden your search.</p>");
-    }
-    else {
-      var row_content;
-      $.get( "../templates/table-row.ejs", function( template ) {
-          for (idx in SearchableMapLib.currentResults.features) {
-            row_content = ejs.render(template, {obj: SearchableMapLib.currentResults.features[idx].properties});
-
-            results.append(row_content);
-          }
-        });
-      }
-  },
-
-  getResults: function() {
-    if (SearchableMapLib.debug) {
-      console.log('results length')
-      console.log(SearchableMapLib.currentResults.features.length)
-    }
-
-    var recname = SearchableMapLib.recordNamePlural;
-    if (SearchableMapLib.currentResults.features.length == 1) {
-        recname = SearchableMapLib.recordName;
-    }
-
-    SearchableMapLib.results_div.update(SearchableMapLib.currentResults.features.length);
-
-    $('#list-result-count').html(SearchableMapLib.currentResults.features.length.toLocaleString('en') + ' ' + recname + ' found')
-  },
-
-  modalPop: function(data) {
-    if (SearchableMapLib.debug) {
-      console.log('launch modal')
-      console.log(data);
-    }
-    var modal_content;
-    $.get( "../templates/popup.ejs", function( template ) {
-        modal_content = ejs.render(template, {obj: data});
-        $('#modal-pop').modal();
-        $('#modal-main').html(modal_content);
-    });
-  },
-
-  clearSearch: function(){
-    if (SearchableMapLib.currentResultsLayer) {
-      SearchableMapLib.currentResultsLayer.remove();
-    }
-    if (SearchableMapLib.centerMark)
-      SearchableMapLib.map.removeLayer( SearchableMapLib.centerMark );
-    if (SearchableMapLib.radiusCircle)
-      SearchableMapLib.map.removeLayer( SearchableMapLib.radiusCircle );
-  },
-
-  createSQL: function() {
-    var address = $("#search-address").val();
-
-    // this is a fun hack to do a deep copy of the GeoJSON data
-    SearchableMapLib.currentResults = JSON.parse(JSON.stringify(SearchableMapLib.geojsonData));
-
-    if(SearchableMapLib.currentPinpoint != null && address != '') {
-        var point = turf.point([SearchableMapLib.currentPinpoint[1], SearchableMapLib.currentPinpoint[0]]);
-        var buffered = turf.buffer(point, SearchableMapLib.radius, {units: 'meters'});
-
-        SearchableMapLib.currentResults = turf.pointsWithinPolygon(SearchableMapLib.currentResults, buffered);
-
-        if (SearchableMapLib.debug) {
-          console.log('found points within')
-          console.log(SearchableMapLib.currentResults);
-        }
-    }
-
-    //-----custom filters-----
-
-    //-----name search filter-----
-    var name_search = $("#search-name").val().replace("'", "\\'");
-    if (name_search != '') {
-      SearchableMapLib.currentResults.features = $.grep(SearchableMapLib.currentResults.features, function(r) {
-          return r.properties["Facility Name"].toLowerCase().indexOf(name_search.toLowerCase()) > -1;
-        });
-    }
-    //-----end name search filter-----
-
-    // -----end of custom filters-----
-
-    SearchableMapLib.currentResultsLayer = L.geoJSON(SearchableMapLib.currentResults, {
-        pointToLayer: function (feature, latlng) {
-          return L.marker(latlng, {icon: SearchableMapLib.getIcon(feature.properties["Type"])} );
+    if (fileType === 'csv') {
+      Papa.parse(filePath, {
+        download: true,
+        header: true,
+        complete: function (result) {
+          dataRecords = result.data;
+          processRecords();
         },
-        onEachFeature: onEachFeature
-      }
-    );
-
-    //messy - clean this up later
-    function onEachFeature(feature, layer) {
-      layer.on({
-        mouseover: hoverFeature,
-        mouseout: removeHover,
-        click: modalPop
+        error: function (error) {
+          console.error("Error loading CSV:", error);
+        }
       });
+    } else {
+      console.error("Unsupported file type:", fileType);
     }
+  }
 
-    function hoverFeature(e) {
-      SearchableMapLib.info.update(e.target.feature.properties);
-    }
+  function processRecords() {
+    dataRecords.forEach(function (record) {
+      if (record.Latitude && record.Longitude) {
+        var marker = L.marker([record.Latitude, record.Longitude], {
+          title: record.Title || "Untitled"
+        });
 
-    function removeHover(e) {
-      SearchableMapLib.info.update();
-    }
-
-    function modalPop(e) {
-      SearchableMapLib.modalPop(e.target.feature.properties)
-    }
-
-  },
-
-  setZoom: function() {
-    var zoom = '';
-    if (SearchableMapLib.radius >= 8050) zoom = 12; // 5 miles
-    else if (SearchableMapLib.radius >= 3220) zoom = 13; // 2 miles
-    else if (SearchableMapLib.radius >= 1610) zoom = 14; // 1 mile
-    else if (SearchableMapLib.radius >= 805) zoom = 15; // 1/2 mile
-    else if (SearchableMapLib.radius >= 400) zoom = 16; // 1/4 mile
-    else zoom = 16;
-
-    SearchableMapLib.map.setView(new L.LatLng( SearchableMapLib.currentPinpoint[0], SearchableMapLib.currentPinpoint[1] ), zoom)
-  },
-
-  addIcon: function() {
-    SearchableMapLib.centerMark = new L.Marker(SearchableMapLib.currentPinpoint, { icon: (new L.Icon({
-            iconUrl: '/img/blue-pushpin.png',
-            iconSize: [32, 32],
-            iconAnchor: [10, 32]
-    }))});
-
-    SearchableMapLib.centerMark.addTo(SearchableMapLib.map);
-  },
-
-  addCircle: function() {
-    SearchableMapLib.radiusCircle = L.circle(SearchableMapLib.currentPinpoint, {
-        radius: SearchableMapLib.radius,
-        fillColor:'#1d5492',
-        fillOpacity:'0.1',
-        stroke: false,
-        clickable: false
+        marker.bindPopup(renderHoverContent(record));
+        markers.addLayer(marker);
+      }
     });
 
-    SearchableMapLib.radiusCircle.addTo(SearchableMapLib.map);
-  },
+    map.addLayer(markers);
+  }
 
-  //converts a slug or query string in to readable text
-  convertToPlainString: function(text) {
-    if (text == undefined) return '';
-    return decodeURIComponent(text);
-  },
+  function renderHoverContent(record) {
+    return `
+      <div class="hover-box">
+        <strong>${record["Title"] || "N/A"}</strong><br />
+        ${record["YouTube Clip ID"] ? `
+          <iframe width="200" height="113" 
+            src="https://www.youtube.com/embed/${record["YouTube Clip ID"]}" 
+            title="YouTube video player" frameborder="0" allowfullscreen></iframe><br />`
+          : record["Archive.org Clip ID"] ? `
+          <iframe width="200" height="113" 
+            src="https://archive.org/embed/${record["Archive.org Clip ID"]}" 
+            title="Archive.org video player" frameborder="0" allowfullscreen></iframe><br />`
+          : record["Image URL"] ? `
+          <img src="${record["Image URL"]}" alt="Image of ${record["Title"]}" 
+            style="width: 100px; height: auto; margin-top: 5px;"><br />`
+          : ""}
+        <strong>Release Year:</strong> ${record["Release Year"] || "N/A"}<br />
+        <strong>Location:</strong> ${record["Location"] || "N/A"}<br />
+        <strong>Director:</strong> ${record["Director"] || "N/A"}<br />
+        ${record["Language"] ? `<strong>Language:</strong> ${record["Language"]}<br />` : ""}
+        ${record["Length"] ? `<strong>Length:</strong> ${record["Length"]}<br />` : ""}
+        <strong>Description:</strong> ${record["Description"] || "N/A"}<br />
+        ${record["Website URL"] ? `
+          <strong>Additional Information:</strong> 
+          <a href="${record["Website URL"]}" target="_blank">Visit Website</a><br />`
+          : ""}
+        ${record["Rights"] ? `
+          <strong>Rights:</strong> 
+          ${record["Rights"] === "public domain" ? `
+            <a href="http://rightsstatements.org/vocab/NoC-US/1.0/" target="_blank">Public Domain</a>`
+            : record["Rights"] === "non-commercial" ? `
+            <a href="http://rightsstatements.org/vocab/NoC-NC/1.0/" target="_blank">Non-Commercial</a>`
+            : ""}
+          <br />`
+          : ""}
+      </div>
+    `;
+  }
 
-  // -----custom functions-----
-  getIcon: function(type){
-    if (type == "ocean") return redIcon;
-    if (type == "freshwater") return blueIcon;
-    if (type == "manmade") return yellowIcon;
-    return greenIcon;
-  },
-  // -----end custom functions-----
+  function doSearch() {
+    var query = document.getElementById('search-name').value.toLowerCase();
+    var address = document.getElementById('search-address').value.toLowerCase();
 
-}
+    markers.clearLayers();
+
+    dataRecords.forEach(function (record) {
+      var matchesQuery = record.Title && record.Title.toLowerCase().includes(query);
+      var matchesAddress = record.Location && record.Location.toLowerCase().includes(address);
+
+      if (matchesQuery && matchesAddress && record.Latitude && record.Longitude) {
+        var marker = L.marker([record.Latitude, record.Longitude], {
+          title: record.Title || "Untitled"
+        });
+
+        marker.bindPopup(renderHoverContent(record));
+        markers.addLayer(marker);
+      }
+    });
+
+    map.addLayer(markers);
+  }
+
+  return {
+    initialize: initialize,
+    doSearch: doSearch
+  };
+})();
